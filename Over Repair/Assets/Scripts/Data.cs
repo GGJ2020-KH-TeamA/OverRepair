797380770eb1bf2bf1d138d69225ecc8f86aef20
Huashen87 <60507281+Huashen87@users.noreply.github.com>
2020-02-05 02:40:06 +0800

    難度控制器(尚未實裝)

---
+++ Over Repair/Assets/Scripts/Data.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    //Player
+    public bool[] PlayerInitStates;
+    public float PlayerInitSpeed;
+    public float PlayerFootSpeed;
+    public int PlayerBlindLevel;
+    public int PlayerBreakConstant;
+    public int PlayerMaxBreakProbability;
+
+    //RobotDown
+    public bool[] RobotDownInitStates;
+
+    //Item
+    public float ConveyorSpeed;
+    public float ItemSpanSpace;
+    public float NeedItemIncrease;
+
+    //Timer
+    public int MaxBatteryTime;
+}
---
+++ Over Repair/Assets/Scripts/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Data data;
+
+    public void EasyMode()
+    {
+        data.PlayerInitStates = new bool[] { true, true, true, true, true, true };
+        data.PlayerInitSpeed = 2f;
+        data.PlayerFootSpeed = 1f;
+        data.PlayerBlindLevel = 120;
+        data.PlayerBreakConstant = 3;
+        data.PlayerMaxBreakProbability = 50;
+
+        data.RobotDownInitStates = new bool[] { true, true, true, true, true, false };
+
+        data.ConveyorSpeed = 1f;
+        data.ItemSpanSpace = 2f;
+        data.NeedItemIncrease = 50;
+
+        data.MaxBatteryTime = 40;
+    }
+
+    public void NormalMode()
+    {
+        data.PlayerInitStates = new bool[] { true, true, true, true, true, true };
+        data.PlayerInitSpeed = 2f;
+        data.PlayerFootSpeed = 1f;
+        data.PlayerBlindLevel = 120;
+        data.PlayerBreakConstant = 3;
+        data.PlayerMaxBreakProbability = 50;
+
+        data.RobotDownInitStates = new bool[] { true, true, true, true, true, false };
+
+        data.ConveyorSpeed = 1f;
+        data.ItemSpanSpace = 2f;
+        data.NeedItemIncrease = 50;
+
+        data.MaxBatteryTime = 40;
+    }
+
+    public void HardMode()
+    {
+        data.PlayerInitStates = new bool[] { true, true, true, true, true, true };
+        data.PlayerInitSpeed = 2f;
+        data.PlayerFootSpeed = 1f;
+        data.PlayerBlindLevel = 120;
+        data.PlayerBreakConstant = 3;
+        data.PlayerMaxBreakProbability = 50;
+
+        data.RobotDownInitStates = new bool[] { true, true, true, true, true, false };
+
+        data.ConveyorSpeed = 1f;
+        data.ItemSpanSpace = 2f;
+        data.NeedItemIncrease = 50;
+
+        data.MaxBatteryTime = 40;
+    }
+}
